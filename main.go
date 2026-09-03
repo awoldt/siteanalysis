3ce0e5197404fbb30f0e18dc1dffec5f476843bc
awoldt <abreckwoldt23@gmail.com>
2026-09-03 14:45:05 -0400

    production ready

--- main.go
+++ main.go
@@ -6,10 +6,18 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
+
+func serveFile(path string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, path)
+	}
+}
 
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		file, err := os.ReadFile("index.html")
@@ -22,6 +30,14 @@ func main() {
 		w.Header().Set("content-type", "text/html")
 		w.Write(file)
 	})
+
+	r.Get("/favicon-96x96.png", serveFile("favicon-96x96.png"))
+	r.Get("/favicon.svg", serveFile("favicon.svg"))
+	r.Get("/favicon.ico", serveFile("favicon.ico"))
+	r.Get("/apple-touch-icon.png", serveFile("apple-touch-icon.png"))
+	r.Get("/site.webmanifest", serveFile("site.webmanifest"))
+	r.Get("/web-app-manifest-192x192.png", serveFile("web-app-manifest-192x192.png"))
+	r.Get("/web-app-manifest-512x512.png", serveFile("web-app-manifest-512x512.png"))
 
 	r.Get("/api", func(w http.ResponseWriter, r *http.Request) {
 		validUrl, err := extractAbsoluteSiteQuery(r.RequestURI)
